268d3ebf73abdda0ea55608c8f59f53542c959f7
kkreal <kylekreal@gmail.com>
2025-05-20 16:07:48 -0700

    added iframe file, partailly added panic key & site

---
+++ frontend-files/js/panic.js
@@ -0,0 +1,15 @@
+let site = localStorage.getItem('panicsite');
+let key = localStorage.getItem('panickey');
+
+if (!site) {
+    site = "https://www.google.com/";
+}
+if (!key) {
+    key = "!";
+}
+
+document.addEventListener("keypress", function(evt) {
+    if (evt.key === key) {
+        window.location.href = site;
+    }
+});
