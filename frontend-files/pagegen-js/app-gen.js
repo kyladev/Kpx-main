ee809b1913adc491f81f1863d32d839658962776
kkreal <kylekreal@gmail.com>
2025-06-04 21:00:45 -0700

    Added more JS generation

---
+++ frontend-files/pagegen-js/app-gen.js
@@ -0,0 +1,8 @@
+create_section("apps/view?q=https://github.com/", "/frontend-files/assets/githublogo.png", "Github");
+create_section("apps/view?q=https://win11.blueedge.me/", "/frontend-files/assets/windows.jpg", "Windows 11 in React");
+create_section("apps/view?q=https://xp.quenq.com/", "/frontend-files/assets/windowsxp.png", "Windows XP");
+create_section("apps/view?q=https://chatgpt.com/", "/frontend-files/assets/chatgpt-logo.jpg", "ChatGPT");
+create_section("apps/view?q=https://www.twitch.tv/", "/frontend-files/assets/twitch-logo.png", "Twitch");
+create_section("apps/view?q=https://discord.com/", "/frontend-files/assets/discord-logo.png", "Discord");
+create_section("apps/view?q=https://open.spotify.com/", "/frontend-files/assets/spotify.png", "Spotify");
+create_section("apps/view?q=https://listen.tidal.com/", "/frontend-files/assets/tidal.png", "Tidal");
