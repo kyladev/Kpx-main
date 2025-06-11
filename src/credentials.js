ffb3f14ebcb13248264afe96a06795db30e111a4
kkreal <kylekreal@gmail.com>
2025-06-11 09:03:56 -0700

    Updated credentials
    created private repo to store credentials

--- src/credentials.js
+++ src/credentials.js
@@ -1,15 +1,3 @@
 export const users = {
-    "admin": "hwack5983",
-    "GrasBiter": "ilovekieran@12k", //Tushita
-    "BrandonLeung": "Brandon2011!", //Brandon note: paid
-    "Talon-1": "s#14759", //Kostya note: paid
-    "Reid_Eats_Pirates": "ThisIsAP@ssword", //Reid note: paid
-    //"Ramenonoodle": "/hpMAR!o", //Rom note: awaiting payment
-    "worst_player": "aaaaaaa.-", //Pete note: paid
-    "ryanandersenj6": "ryananderseni7", //Ryan note: paid
-    "Best_player": "brandonispoop", //Milo note: paid
-    "Sathvik123": "$Sathvik19", //Sathvik note: paid
-    "dillydoo": "currydagoat", //Dillon note: paid
-    //"Kaden": "Kaden0510$", // Kaden paid until 6/7
-    "Braxtonlikesmilk": "Toyman2341" //Eddie paid until 6/14
+    "admin": "hwack5983"
 };
