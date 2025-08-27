e3c35b706fc9337466ba672d27070f878ed71a3e
kkreal <kylekreal@gmail.com>
2025-08-26 17:40:48 -0700

    Cleaned up code

--- src/resources-router.js
+++ src/resources-router.js
@@ -4,25 +4,25 @@ export default async function resources_router(fastify) {
     fastify.get("/", {
         preHandler: authMiddleware(),
         handler: (req, res) => {
-            return res.sendFile("main/files/pages/resources.html", publicPath);
+            return res.sendFile("main/pages/resources.html", publicPath);
         }
     });
     fastify.get("/g", {
         preHandler: authMiddleware(),
         handler: (req, res) => {
-            return res.sendFile("main/files/pages/resource-pages/guides.html", publicPath);
+            return res.sendFile("main/pages/resource-pages/guides.html", publicPath);
         }
     });
     fastify.get("/l", {
         preHandler: authMiddleware(),
         handler: (req, res) => {
-            return res.sendFile("main/files/pages/resource-pages/links.html", publicPath);
+            return res.sendFile("main/pages/resource-pages/links.html", publicPath);
         }
     });
     fastify.get("/s", {
         preHandler: authMiddleware(),
         handler: (req, res) => {
-            return res.sendFile("main/files/pages/resource-pages/sites.html", publicPath);
+            return res.sendFile("main/pages/resource-pages/sites.html", publicPath);
         }
     });
 }
